d9192d8a525b978b0bb0ed3afbe677abe90a3060
avipowar <powaravinash06/2gmail.com>
2026-06-27 09:22:38 +0530

    create server

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT ?? 8000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "server is healthy" });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on PORT ${PORT}`);
+});
