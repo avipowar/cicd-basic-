bca7abd222ce59d1d66bf8d6042480ea206a8097
avipowar <powaravinash06/2gmail.com>
2026-06-27 18:18:27 +0530

    add version v1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8000;
 
 app.get("/", (req, res) => {
-  res.json({ message: "server is healthy" });
+  res.json({ message: "server is healthy v1" });
 });
 
 app.listen(PORT, () => {
